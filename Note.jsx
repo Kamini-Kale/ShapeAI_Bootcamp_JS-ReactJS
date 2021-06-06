8d4cf44d382cf4e82c3fafcbc490c647f2ba69fc
Kamini-Kale <85411538+Kamini-Kale@users.noreply.github.com>
2021-06-06 13:03:26 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className=" note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        -------------FREE--------------- JS & REACT.JS Bootcamp --Starts 25 July
+        @6.30pm-- --------Register yourself------ NO Prerequisites Required.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
